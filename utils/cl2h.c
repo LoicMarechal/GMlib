26691e7c89c22a49f3600b8ab70fcf06ee7a30df
Loïc Maréchal <marechal@sar.saclay.inria.fr>
2017-02-02 10:28:02 +0100

    Preparation phase befor moving the project to CMake

--- utils/cl2h.c
+++ utils/cl2h.c
@@ -3,62 +3,62 @@
 #include <stdio.h>
 #include <string.h>
 
-main(int ArgCnt, char **ArgVec)
+int main(int ArgCnt, char **ArgVec)
 {
-	char c, *InpNam, *OutNam, *PrcNam;
-	int i;
-	FILE *InpHdl, *OutHdl;
+   char c, *InpNam, *OutNam, *PrcNam;
+   int i;
+   FILE *InpHdl, *OutHdl;
 
-	if(ArgCnt != 4)
-	{
-		puts("Usage : cl2h source.cl destination.h procedure");
-		exit(1);
-	}
+   if(ArgCnt != 4)
+   {
+      puts("Usage : cl2h source.cl destination.h procedure");
+      exit(1);
+   }
 
-	InpNam = *++ArgVec;
-	OutNam = *++ArgVec;
-	PrcNam = *++ArgVec;
+   InpNam = *++ArgVec;
+   OutNam = *++ArgVec;
+   PrcNam = *++ArgVec;
 
-	if(!(InpHdl = fopen(InpNam, "r")))
-	{
-		printf("Cannot open %s for reading\n", InpNam);
-		exit(1);
-	}
+   if(!(InpHdl = fopen(InpNam, "r")))
+   {
+      printf("Cannot open %s for reading\n", InpNam);
+      exit(1);
+   }
 
-	if(!(OutHdl = fopen(OutNam, "w")))
-	{
-		printf("Cannot open %s for writing\n", OutNam);
-		exit(1);
-	}
+   if(!(OutHdl = fopen(OutNam, "w")))
+   {
+      printf("Cannot open %s for writing\n", OutNam);
+      exit(1);
+   }
 
-	fprintf(OutHdl, "char *%s = \"\\n\" \\\n", PrcNam);
+   fprintf(OutHdl, "char *%s = \"\\n\" \\\n", PrcNam);
 
-	do
-	{
-		fprintf(OutHdl, "\"");
+   do
+   {
+      fprintf(OutHdl, "\"");
 
-		do
-		{
-			c = fgetc(InpHdl);
+      do
+      {
+         c = fgetc(InpHdl);
 
-			if(c == '\\')
-			do
-			{
-				c = fgetc(InpHdl);
-			}while(c == '\n');
-			
-			if((c == EOF) || (c == '\n'))
-				break;
+         if(c == '\\')
+         do
+         {
+            c = fgetc(InpHdl);
+         }while(c == '\n');
+         
+         if((c == EOF) || (c == '\n'))
+            break;
 
-			fputc(c, OutHdl);
-		}while((c != EOF) && (c != '\n'));
-		fprintf(OutHdl, "\\n\" \\\n");
-	}while(c != EOF);
+         fputc(c, OutHdl);
+      }while((c != EOF) && (c != '\n'));
+      fprintf(OutHdl, "\\n\" \\\n");
+   }while(c != EOF);
 
-	fprintf(OutHdl, "\n;");
+   fprintf(OutHdl, "\n;");
 
-	fclose(InpHdl);
-	fclose(OutHdl);
+   fclose(InpHdl);
+   fclose(OutHdl);
 
-	exit(0);
+   exit(0);
 }
